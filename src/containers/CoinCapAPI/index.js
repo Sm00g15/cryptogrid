979b5f76d792e99fd2dd7145b7e1a31d0f996036
Sean Stanley <sean@foodcoop.org.nz>
2017-06-30 15:40:19 +1200

    sockets and fetching data initially working

--- src/containers/CoinCapAPI/index.js
+++ src/containers/CoinCapAPI/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import io from 'socket.io-client';
+import 'whatwg-fetch'; // makes 'fetch' work in all browsers https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
 import CoincapTable from '../../components/CoincapTable';
 
+const apiURL = 'http://socket.coincap.io';
+const socket = io(apiURL);
 
 export default class CoinCapAPI extends React.PureComponent {
   constructor() {
     super();
     this.state = {};
+
+    fetch(`${apiURL}/front`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+    // socket.on('trades', (tradesData) => {
+    //   console.log(data);
+    //   // occur very often and very fast
+    //   // tradesData contains info on a trade. You should update the table with this info.
+    // });
+    socket.on('global', (data) => {
+      console.log(data);
+      // occur more slowly
+      // data contains the global message data. Not entirely sure what it's used for.
+    });
   }
   render() {
-    const socket = io('http://socket.coincap.io');
-    socket.on('connection', () => console.log(socket.id));
-    socket.on('/coins', (data) => {
-      this.setState(data);
-      console.log(data);
-    });
     return (
       <div>
         <CoincapTable data={this.state} />
