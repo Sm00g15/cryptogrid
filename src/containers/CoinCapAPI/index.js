b20fda758af008b9ca5ff0a874080c039781d51b
Sean Stanley <sean@foodcoop.org.nz>
2017-06-30 13:17:26 +1200

    babel setup and file structure setup

---
+++ src/containers/CoinCapAPI/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default class CoinCapAPI extends React.pureComponent {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        {/*  put dumb component here */}
+        hello!
+      </div>
+    );
+  }
+}
