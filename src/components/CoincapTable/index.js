e54afe7c3b913af28e594606cf5c1c5fc631da9b
Sean M <smagin07@Seans-MacBook-Air.local>
2017-07-02 18:00:43 +1200

    added in data grid, needds to be double-checked

--- src/components/CoincapTable/index.js
+++ src/components/CoincapTable/index.js
@@ -6,13 +6,27 @@ export default function CoincapTable(props) {
   console.log(props);
   return (
     <div>
-      {props.data &&
-        <div>
-          <h1>It Works!</h1>
-          <p>Enjoy!</p>
-        </div>
-      }
-
+      <table>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>%24hr</th>
+          <th>Market Cap</th>
+          <th>Available Supply</th>
+          <th>24 Hour Volume</th>
+          <th></th>
+        </tr>
+        <tr>
+          <td>{data.position24}</td>
+          <td>{data.long}</td>
+          <td>{data.price}</td>
+          <td>{data.cap24hrChange}</td>
+          <td>{data.mktcap}</td>
+          <td>{data.supply}</td>
+          <td>{data.volume}</td>
+        </tr>
+      </table>
     </div>
   );
 }
